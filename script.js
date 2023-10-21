3ed06f5ac1426699174f1aff4d41ef036ea36cb5
Utkarsh <93548048+UtkarshShah0@users.noreply.github.com>
2023-10-21 23:36:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,30 +3,53 @@ document.addEventListener('DOMContentLoaded', function () {
     const chatButton = document.getElementById('chat-button');
     const summaryButton = document.getElementById('summary-button');
     const imageButton = document.getElementById('image-button');
+    const webButton = document.getElementById('web-button');
 
     const chatSection = document.getElementById('chat-section');
     const summarySection = document.getElementById('summary-section');
     const imageSection = document.getElementById('image-section');
+    const webSection = document.getElementById('web-section');
 
-    //Code to hide and unhide sections
-    
     chatButton.addEventListener('click', function () {
         chatSection.style.display = 'block';
         summarySection.style.display = 'none';
         imageSection.style.display = 'none';
+        webSection.style.display = 'none';
     });
 
     summaryButton.addEventListener('click', function () {
         chatSection.style.display = 'none';
         summarySection.style.display = 'block';
         imageSection.style.display = 'none';
+        webSection.style.display = 'none';
     });
 
     imageButton.addEventListener('click', function () {
         chatSection.style.display = 'none';
         summarySection.style.display = 'none';
         imageSection.style.display = 'block';
+        webSection.style.display = 'none';
     });
+
+    webButton.addEventListener('click', function () {
+        chatSection.style.display = 'none';
+        summarySection.style.display = 'none';
+        imageSection.style.display = 'none';
+        webSection.style.display = 'block';
+    });
+
+    // Rest of your code...
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("chat-section");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+//   }
 
 // chat section
 
@@ -71,6 +94,25 @@ document.addEventListener('DOMContentLoaded', function () {
                     responseDiv.innerHTML= htmlText;
                     //loading remove
                     rotate.classList.remove("rainbow")
+
+                    const copyButton = document.getElementById('chat-copyButton');
+
+                    copyButton.addEventListener('click', function () {
+                        // Select the response text
+                        const responseText = responseDiv.innerText;
+                        const tempTextArea = document.createElement('textarea');
+                        tempTextArea.value = responseText;
+                        document.body.appendChild(tempTextArea);
+                        tempTextArea.select();
+                        document.execCommand('copy');
+                        document.body.removeChild(tempTextArea);
+        
+                        // Change button text to indicate successful copy
+                        copyButton.innerText = "✅";
+                        setTimeout(() => {
+                            copyButton.innerText = '📋';
+                        }, 1500); // Reset button text after 1.5 seconds
+                    });
                 } else {
                     responseDiv.textContent = 'Error: Unable to fetch response.';
 
@@ -122,6 +164,25 @@ document.addEventListener('DOMContentLoaded', function () {
             //loading remove
             rotate.classList.remove("rainbow")
 
+            const copyButton = document.getElementById('summary-copyButton');
+
+            copyButton.addEventListener('click', function () {
+                // Select the response text
+                const responseText = responseDiv_2.innerText;
+                const tempTextArea = document.createElement('textarea');
+                tempTextArea.value = responseText;
+                document.body.appendChild(tempTextArea);
+                tempTextArea.select();
+                document.execCommand('copy');
+                document.body.removeChild(tempTextArea);
+
+                // Change button text to indicate successful copy
+                copyButton.innerText = "✅";
+                setTimeout(() => {
+                    copyButton.innerText = '📋';
+                }, 1500); // Reset button text after 1.5 seconds
+            });
+
         } else {
             responseDiv_2.textContent = 'Error: Unable to fetch response.';
 
@@ -132,7 +193,7 @@ document.addEventListener('DOMContentLoaded', function () {
     //Image Section
 
     const questionInput_3 = document.getElementById('question_image');
-    const dataInput_3 = document.getElementById('data_image');
+    // const dataInput_3 = document.getElementById('data_image');
     const submitButton_3 = document.getElementById('submit_image');
     const responseDiv_3 = document.getElementById('response_image');
 
@@ -143,7 +204,7 @@ document.addEventListener('DOMContentLoaded', function () {
         rotate.classList.add("rainbow")
 
         const base = questionInput_3.value.trim();
-        const extra = dataInput_3.value.trim();
+        // const extra = " ";
         
         
         
@@ -153,7 +214,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify({ base, extra })
+                    body: JSON.stringify({ base })
                 });
 
         if (response.ok) {
@@ -183,24 +244,57 @@ document.addEventListener('DOMContentLoaded', function () {
             input.value = url;
             input.size = "30";
             input.style.textOverflow = "ellipsis";
-          
-            // Create a new button element
-            var button = document.createElement("button");
-            button.innerHTML = "Copy";
-            button.classList.add("submit")
-            button.onclick = function() {
-              input.select();
-              document.execCommand("copy");
-            };
-          
-            // Append the input and button elements to the url-container div
+
             var container = document.getElementById("response_image");
 
+            responseDiv_3.innerText = ""
+
             container.appendChild(input);
-            container.appendChild(button)
+
+            const copyButton = document.getElementById('image-copyButton');
+
+            copyButton.addEventListener('click', function () {
+                // Select the response text
+                const responseText = url;
+                const tempTextArea = document.createElement('textarea');
+                tempTextArea.value = responseText;
+                document.body.appendChild(tempTextArea);
+                tempTextArea.select();
+                document.execCommand('copy');
+                document.body.removeChild(tempTextArea);
+
+                // Change button text to indicate successful copy
+                copyButton.innerText = "✅";
+                setTimeout(() => {
+                    copyButton.innerText = '📋';
+                }, 1500); // Reset button text after 1.5 seconds
+            });
+
+            // var input = document.createElement("input");
+            // input.type = "text";
+            // input.value = url;
+            // input.size = "30";
+            // input.style.textOverflow = "ellipsis";
+          
+            // // Create a new button element
+            // var button = document.createElement("button");
+            // button.innerHTML = "Copy";
+            // button.classList.add("submit")
+            // button.onclick = function() {
+            //   input.select();
+            //   document.execCommand("copy");
+            // };
+          
+            // // Append the input and button elements to the url-container div
+            // var container = document.getElementById("response_image");
+
+            // container.appendChild(input);
+            // container.appendChild(button)
 
 
             rotate.classList.remove("rainbow")
+
+
 
             // container.appendChild(button)
             // responseDiv_3.innerHTML= htmlText;
@@ -209,6 +303,79 @@ document.addEventListener('DOMContentLoaded', function () {
             
         } else {
             responseDiv_3.textContent = 'Error: Unable to fetch response.';
+
+        }
+
+    });
+
+    //Web Section
+
+    const questionInput_4 = document.getElementById('question_web');
+    const dataInput_4 = document.getElementById('data_web');
+    const submitButton_4 = document.getElementById('submit_web');
+    const responseDiv_4 = document.getElementById('response_web');
+
+    submitButton_4.addEventListener('click', async function () {
+
+        responseDiv_4.innerText = "Your response will be shown here..."
+        //loading
+        rotate.classList.add("rainbow")
+
+        const question = questionInput_4.value;
+        const context = dataInput_4.value;
+        console.log(context)
+        
+        
+        const apiUrl = 'http://127.0.0.1:8000/web'; 
+        const response = await fetch(apiUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ question, context })
+                });
+
+        if (response.ok) {
+            const data = await response.json();
+            const markdownText = data;
+            console.log(data)
+
+            console.log('Original Markdown:', markdownText)
+
+            const markedOptions ={
+                sanitize: false
+            }
+            const htmlText = marked(markdownText, markedOptions);
+
+            console.log('HTML Output:', htmlText);
+            responseDiv_4.innerHTML= htmlText;
+
+
+            const copyButton = document.getElementById('web-copyButton');
+
+            copyButton.addEventListener('click', function () {
+                // Select the response text
+                const responseText = responseDiv_4.innerText;
+                const tempTextArea = document.createElement('textarea');
+                tempTextArea.value = responseText;
+                document.body.appendChild(tempTextArea);
+                tempTextArea.select();
+                document.execCommand('copy');
+                document.body.removeChild(tempTextArea);
+
+                // Change button text to indicate successful copy
+                copyButton.innerText = "✅";
+                setTimeout(() => {
+                    copyButton.innerText = '📋';
+                }, 1500); // Reset button text after 1.5 seconds
+            });
+
+            
+            //loading remove
+            rotate.classList.remove("rainbow")
+
+        } else {
+            responseDiv_4.textContent = 'Error: Unable to fetch response.';
 
         }
     });
